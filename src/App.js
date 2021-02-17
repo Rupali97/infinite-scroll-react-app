2f5bcdb1e8453c16c7f1e6615f0f9a59a1f6d7ec
Rupali Subhash Doke <rupalidoke97@gmail.com>
2021-02-18 01:26:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react"
+
+import "./style.css"
 
 function App() {
+const [boxes, setBoxes] = useState([{bg: "#777"}])
+
+useEffect(() => {
+
+  const interval = setInterval(() => {
+    setBoxes([...boxes, {bg: "#" + (( Math.random() * 0xffffff) << 0).toString(16) }])
+  }, 500)
+
+  return () => clearInterval(interval)
+ 
+}, [boxes])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero">
+      {boxes.map((box, i) => (
+        <div key={i} className="box" style={{backgroundColor: box.bg}} >
+          <p>{i}</p>
+        </div>
+      ))}
     </div>
   );
 }
